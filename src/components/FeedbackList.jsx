e49055a9694be6af50ed89990bf7ed06e6060fd8
Elvan Hidayat <elvanhd@gmail.com>
2024-01-06 23:01:51 +0700

    delete event

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,18 +1,18 @@
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
-    return <p>No Feedback Yet</p>;
+    return <p>No Feedback Yet</p>
   }
 
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
-  );
+  )
 }
 
 FeedbackList.propTypes = {
@@ -21,8 +21,8 @@ FeedbackList.propTypes = {
       id: PropTypes.number.isRequired,
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
-    }),
+    })
   ),
-};
+}
 
-export default FeedbackList;
+export default FeedbackList
